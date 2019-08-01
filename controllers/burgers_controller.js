7afcfab2e8dcf70f638a924abf89dc3e886df257
Hunter Nix <huntercnix@gmail.com>
2019-07-31 23:52:39 -0500

    the page is rendering but i cant eat the burgers or post a new burger

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,13 +2,30 @@ var express = require("express");
 var burger = require("../models/burger.js");
 var router = express.Router();
 
-router.get("/", function(req, res){
+router.get("/", function (req, res) {
   burger.selectAll(function (data) {
     var hbsObject = {
       burgers: data
     };
+
     console.log(hbsObject);
     res.render("index", hbsObject);
+  });
+});
+
+router.post("/", function (req, res) {
+  burger.insertOne(req.body.burger_name, function () {
+    res.redirect("/");
+  });
+});
+
+router.put("/:id", function (req,res) {
+  var id = req.params.id;
+
+  console.log("id", id);
+
+  burger.updateOne(id, function() {
+    res.redirect("/");
   });
 });
 
